837629e6b27d15777395bded1ab5f1d410d10f15
Dev5H0 <thorhallur30102004@gmail.com>
2021-01-09 18:22:15 +0000

    Added tabs.
    Added tabs, the auto clicker and the enable/disable button still don't work...

--- Code/main.py
+++ Code/main.py
@@ -1,100 +1,107 @@
-# <imports>
 import xml.etree.ElementTree as ET
-import keyboard
 
 from time import sleep
 from tkinter import *
 from tkinter import ttk
 from gameConfig import wTitle,wRes
-# </imports>
 
-# <data>
-# <xml setup>
+
 dataFile = "Code/data.xml" # Location of data file
 xmlTree = ET.parse(dataFile) 
 xmlData = xmlTree.find("data") # Looks for the "data" tag
 xmlClicks = xmlData.get("clicks") # Looks for the "clicks" attribute inside of the data tag
+
 xmlUpgrades = xmlTree.find("upgrades")
-# </xml setup>
-# <upgrades>
 ugAutoClick = xmlUpgrades.find("autoClicker")
 xmlAutoClick = ugAutoClick.get("l")
 acPrice = 10
-print(xmlAutoClick)
-# </upgrades>
+
 autoClick = int(xmlAutoClick)
 clicks = int(xmlClicks) # Takes the string from <data clicks="">, converts it into an integer and puts it into a variable
-# </data>
 
-# <tkinter setup>
 root = Tk()
 root.title(wTitle) # Window title, change in "gameConfig.py"
 root.geometry(wRes) # Window size, change in "gameConfig.py"
-# </tkinter setup>
 
-# <tkinter tabs>
-#tkTabs = ttk.Notebook(root)
-#mainTab = ttk.Frame(tkTabs)
-#upgradeTab = ttk.Frame(tkTabs)
+tkTabs = ttk.Notebook(root)
+mainTab = ttk.Frame(tkTabs)
+upgradeTab = ttk.Frame(tkTabs)
+settingsTab = ttk.Frame(tkTabs)
 
-#tkTabs.add(mainTab,text="Main") # Creates a tab called "Main"
-#tkTabs.add(upgradeTab,text="Upgrades") # See above comment
-#tkTabs.pack() # Adds the tabs
+tkTabs.add(mainTab,text="Main") # Creates main tab
+tkTabs.add(upgradeTab,text="Upgrades") # Creates upgrade tab
+tkTabs.add(settingsTab,text="Settings") # Creates settings tab
+tkTabs.pack() # Adds the tabs to the gui
 
-#mainTabFrame = ttk.Frame(tkTabs)
-#upgradeTabFrame = ttk.Frame=(tkTabs)
+upgradesLabel = Label(upgradeTab,text="Upgrades")
+clickLabel = Label(mainTab,text="Clicks: "+str(clicks))
+ugClickLabel = Label(upgradeTab,text="Clicks: "+str(clicks))
 
-#mainTabFrame.pack(fill="both",expand=1)
-#upgradeTabFrame.pack(fill="both",expand=1)
 
-#tkTabs.add(mainTabFrame,text="Main")
-#tkTabs.add(upgradeTabFrame,text="Upgrades")
-# </tkinter tabs>
-
-# <tkinter widgets setup>
-emptyLabel = Label(root,text="")
-
-upgradesLabel = Label(root,text="Upgrades")
-
-clickLabel = Label(root,text="Clicks: "+str(clicks))
 def onMainClick():
    global clicks
    clicks += 1
+   ugClickLabel.config(text="Clicks: "+str(clicks))
    clickLabel.config(text="Clicks: "+str(clicks))
+   ugClickLabel.update_idletasks
    clickLabel.update_idletasks
    sleep(.1)
 def onSave(): # Save progress
    global clicks # Gets the amount clicks 
    global autoClick
-   xmlData.set("clicks",str(clicks)) # Replaces the "clicks" data attribute inside of the data file
-   # <save upgrades>
-   ugAutoClick.set(("l"),str(autoClick))
-   # </save upgrades>
+   xmlData.set("clicks",str(clicks)) # Saves "Clicks"
+   ugAutoClick.set(("l"),str(autoClick)) # Saves "Auto Clicker" upgrade
    xmlTree.write(dataFile) # Saves the data file with the new attribute value
 def upgrade(number):
    global clicks
+   global acPrice
    if clicks >= acPrice:
       global autoClick
       print(clicks)
       autoClick += number
       clicks = clicks-acPrice
       print(clicks)
+      acPrice = int(round(float(acPrice)*float(1.3)))
+      ugClickLabel.config(text="Clicks: "+str(clicks))
       clickLabel.config(text="Clicks: "+str(clicks))
+      ugClickLabel.update_idletasks
       clickLabel.update_idletasks
-clickButton = Button(root,text="Click Me",command=onMainClick)
-upgradeButton = Button(root,text="Auto Clicker "+str(int(autoClick)+1)+": "+str(acPrice)+" Clicks",command=lambda: upgrade(1))
-saveButton = Button(root,text="Save",command=onSave)
+def autoClicking():
+   global acToggle
+   if acToggle == "Enabled":
+      global clicks
+      global autoClick
+      clicks += autoClick
+      ugClickLabel.update_idletasks
+      clickLabel.update_idletasks
+      sleep(1)
+   else: sleep(.5)
+def toggleUpgrade():
+   global acToggle
+   global acToggleButton
+   if acToggle == "Disabled":
+      acToggle = "Enabled"
+      acToggleButton = Button(upgradeTab,text="Auto Clicker "+acToggle,command=toggleUpgrade)
+      acToggleButton.update_idletasks
+   elif acToggle == "Enabled":
+      acToggle = "Disabled"
+      acToggleButton = Button(upgradeTab,text="Auto Clicker "+acToggle,command=toggleUpgrade)
+      acToggleButton.update_idletasks
+clickButton = Button(mainTab,text="Click Me",command=onMainClick)
+saveButton = Button(settingsTab,text="Save",command=onSave)
 
+acButton = Button(upgradeTab,text="Auto Clicker "+str(int(autoClick)+1)+": "+str(acPrice)+" Clicks",command=lambda: upgrade(1))
+acToggle = "Disabled"
+acToggleButton = Button(upgradeTab,text="Auto Clicker "+acToggle,command=toggleUpgrade)
 
-
-clickLabel.pack()
+clickLabel.pack(pady=5)
 clickButton.pack()
-
 saveButton.pack()
 
-emptyLabel.pack()
+ugClickLabel.pack(pady=5)
 upgradesLabel.pack()
-upgradeButton.pack()
-# </tkinter widgets setup>
+acButton.pack()
+if xmlAutoClick >= "1":
+   acToggleButton.pack()
 
 root.mainloop()
