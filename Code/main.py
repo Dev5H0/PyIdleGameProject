90509b73169af8e563e0c597eb72eef23170e5cf
Dev5H0 <thorhallur30102004@gmail.com>
2021-01-10 12:47:22 +0000

    Cleaned Code
    I cleaned the code and added text variables to some of the labels.

--- Code/main.py
+++ Code/main.py
@@ -1,57 +1,47 @@
+# Imports
 import xml.etree.ElementTree as ET
-
 from time import sleep
 from tkinter import *
 from tkinter import ttk
 from gameConfig import wTitle,wRes
 
 
-dataFile = "Code/data.xml" # Location of data file
+# XML
+dataFile = "Code/data.xml"
 xmlTree = ET.parse(dataFile) 
-xmlData = xmlTree.find("data") # Looks for the "data" tag
-xmlClicks = xmlData.get("clicks") # Looks for the "clicks" attribute inside of the data tag
+xmlData = xmlTree.find("data")
+xmlClicks = xmlData.get("clicks")
 
 xmlUpgrades = xmlTree.find("upgrades")
 ugAutoClick = xmlUpgrades.find("autoClicker")
 xmlAutoClick = ugAutoClick.get("l")
 acPrice = 10
 
+
+# Data variables
 autoClick = int(xmlAutoClick)
-clicks = int(xmlClicks) # Takes the string from <data clicks="">, converts it into an integer and puts it into a variable
+clicks = int(xmlClicks)
 
-root = Tk()
-root.title(wTitle) # Window title, change in "gameConfig.py"
-root.geometry(wRes) # Window size, change in "gameConfig.py"
 
-tkTabs = ttk.Notebook(root)
-mainTab = ttk.Frame(tkTabs)
-upgradeTab = ttk.Frame(tkTabs)
-settingsTab = ttk.Frame(tkTabs)
-
-tkTabs.add(mainTab,text="Main") # Creates main tab
-tkTabs.add(upgradeTab,text="Upgrades") # Creates upgrade tab
-tkTabs.add(settingsTab,text="Settings") # Creates settings tab
-tkTabs.pack() # Adds the tabs to the gui
-
-upgradesLabel = Label(upgradeTab,text="Upgrades")
-clickLabel = Label(mainTab,text="Clicks: "+str(clicks))
-ugClickLabel = Label(upgradeTab,text="Clicks: "+str(clicks))
-
+# Functions
+def updateScore():
+   
+   svClicks
+   svClicks.set("Clicks: "+str(clicks))
 
 def onMainClick():
    global clicks
    clicks += 1
-   ugClickLabel.config(text="Clicks: "+str(clicks))
-   clickLabel.config(text="Clicks: "+str(clicks))
-   ugClickLabel.update_idletasks
-   clickLabel.update_idletasks
+   svClicks.set("Clicks: "+str(clicks))
    sleep(.1)
-def onSave(): # Save progress
-   global clicks # Gets the amount clicks 
+
+def onSave():
+   global clicks 
    global autoClick
-   xmlData.set("clicks",str(clicks)) # Saves "Clicks"
-   ugAutoClick.set(("l"),str(autoClick)) # Saves "Auto Clicker" upgrade
-   xmlTree.write(dataFile) # Saves the data file with the new attribute value
+   xmlData.set("clicks",str(clicks))
+   ugAutoClick.set(("l"),str(autoClick))
+   xmlTree.write(dataFile)
+
 def upgrade(number):
    global clicks
    global acPrice
@@ -66,6 +56,7 @@ def upgrade(number):
       clickLabel.config(text="Clicks: "+str(clicks))
       ugClickLabel.update_idletasks
       clickLabel.update_idletasks
+
 def autoClicking():
    global acToggle
    if acToggle == "Enabled":
@@ -76,6 +67,7 @@ def autoClicking():
       clickLabel.update_idletasks
       sleep(1)
    else: sleep(.5)
+
 def toggleUpgrade():
    global acToggle
    global acToggleButton
@@ -88,22 +80,57 @@ def toggleUpgrade():
       acToggleButton = Button(upgradeTab,text="Auto Clicker "+acToggle,command=toggleUpgrade)
       acToggleButton.update_idletasks
 
-      
+
+# Tkinter
+root = Tk()
+root.title(wTitle)
+root.geometry(wRes)
+
+
+# String Variables
+svClicks = StringVar()
+svACUpgrade = StringVar()
+
+
+# Tabs
+tkTabs = ttk.Notebook(root)
+mainTab = ttk.Frame(tkTabs)
+upgradeTab = ttk.Frame(tkTabs)
+settingsTab = ttk.Frame(tkTabs)
+
+tkTabs.add(mainTab,text="Main")
+tkTabs.add(upgradeTab,text="Upgrades")
+tkTabs.add(settingsTab,text="Settings")
+tkTabs.pack()
+
+
+# Main Tab
+clickLabel = Label(mainTab,textvariable=svClicks)
 clickButton = Button(mainTab,text="Click Me",command=onMainClick)
-saveButton = Button(settingsTab,text="Save",command=onSave)
-
-acButton = Button(upgradeTab,text="Auto Clicker "+str(int(autoClick)+1)+": "+str(acPrice)+" Clicks",command=lambda: upgrade(1))
-acToggle = "Disabled"
-acToggleButton = Button(upgradeTab,text="Auto Clicker "+acToggle,command=toggleUpgrade)
-
 clickLabel.pack(pady=5)
 clickButton.pack()
-saveButton.pack()
 
-ugClickLabel.pack(pady=5)
+
+# Upgrades Tab
+upgradesLabel = Label(upgradeTab,text="UPGRADES")
 upgradesLabel.pack()
+
+ugClickLabel = Label(upgradeTab,textvariable=svClicks)
+ugClickLabel.pack(pady=5)
+
+acButton = Button(upgradeTab,text="Auto Clicker "+str(int(autoClick)+1)+": "+str(acPrice)+" Clicks",command=lambda: upgrade(1))
 acButton.pack()
+
+acToggle = "Disabled"
+acToggleButton = Button(upgradeTab,text="Auto Clicker "+acToggle,command=toggleUpgrade)
 if xmlAutoClick >= "1":
    acToggleButton.pack()
+
+
+# Settings Tab
+saveButton = Button(settingsTab,text="Save",command=onSave)
+saveButton.pack()
+
+
 
 root.mainloop()
